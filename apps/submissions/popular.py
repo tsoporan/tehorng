a95d4322aca368a67146d3449a5b4a6683f23d94
Titus Soporan <tsopor@gmail.com>
2011-02-25 23:56:15 -0500

    Finishing get_popular, added cache and Counting support

--- apps/submissions/popular.py
+++ apps/submissions/popular.py
@@ -7,31 +7,79 @@ from django.core import serializers
 
 from tracking.models import Hit
 
-def get_popular(ctype, filterby="hourly"):
+from collections import Counter
+
+from django.contrib.contenttypes.models import ContentType
+from django.core.cache import cache
+
+import pickle
+
+def get_popular(ctype="artist", filterby="hourly"):
     try:
-        ct = ContentType.objects.get(name=ctype)
+        ct = ContentType.objects.get(name__iexact=ctype)
     except:
         return
-
+    
+    counter = Counter()
     now = datetime.now()
     hits = Hit.objects.all()
 
+    loads = pickle.loads
+    dumps = pickle.dumps
+
     if filterby == "hourly":
-        hourago = now - timedelta(hours=1)
-        results = hits.filter(content_type=ct, timestamp__range=(hourago, now))
+        counter.clear()
+    
+        if cache.has_key('popular_hourly'):
+            results = loads(cache.get('popular_hourly'))
+        else:
+            hourago = now - timedelta(hours=1)
+            hits = hits.filter(content_type=ct, timestamp__range=(hourago, now))
+            for obj in [o.content_object for o in hits]:
+                counter[obj] += 1                        
+            results = counter.most_common()[:20]
+            cache.set('popular_hourly', dumps(results), 60*60) #cache for an hour
 
     elif filterby == "daily":
+        counter.clear()
         dayago = now - timedelta(days=1)
-        results = hits.filter(content_type=ct, timestamp__range(dayago, now))
+        
+        if cache.has_key('popular_daily'):
+            results = loads(cache.get('popular_daily'))
+        else:
+            hits  = hits.filter(content_type=ct, timestamp__range=(dayago, now))
+            for obj in [o.content_object for o in hits]:
+                counter[obj] += 1                        
+            results = counter.most_common()[:20]
+            cache.set('popular_daily', dumps(results), 60*60*24) #cache for a day
 
     elif filterby == "monthly":
+        counter.clear()
         monthago = now - timedelta(days=30)
-        results = hits.filter(content_type=ct, timestamp__range(monthago, now))
+        
+        if cache.has_key('popular_monthly'):
+            results = loads(cache.get('popular_monthly'))
+        else:
+            hits = hits.filter(content_type=ct, timestamp__range=(monthago, now))
+            for obj in [o.content_object for o in hits]: 
+                counter[obj] += 1                        
+            results = counter.most_common()[:20]
+            cache.set('popular_monthly', dumps(results), 60*60*24*30) #cache for month
 
     else: #all results
-        results = hits.filter(content_type=ct)
+        counter.clear()
+ 
+        if cache.has_key('popular_alltime'):
+            results = pickle.loads(cache.get('popular_alltime'))
+        else:
+            results = hits.filter(content_type=ct)
+            for obj in [o.content_object for o in results]:
+                counter[obj] += 1                        
+            results = counter.most_common()[:20]
+            cache.set('popular_alltime', pickle.dumps(results), 60*60*24*30) #cache for month
 
-    return serializers.serialize('json', results)       
+    return [obj[0].name for obj in results]
+    #return serializers.serialize('json', results)       
 
 
 
