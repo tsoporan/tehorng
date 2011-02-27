17fbf579e9e17805b501c0336085c00c7ed4578d
Titus Soporan <tsopor@gmail.com>
2011-02-27 02:57:46 -0500

    Rendering get_popular to template

--- apps/submissions/views/popular.py
+++ apps/submissions/views/popular.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 
 from submissions.views.utils import get_popular
 from django.contrib.contenttypes.models import ContentType
+import os
+from django.conf import settings
 
-def popular(request, ctype, filterby):
+def popular(request, ctype, filterby, template="submissions/popular/popular_load.html"):
     content_type = get_object_or_404(ContentType, name__iexact=ctype)
-    json = get_popular(ctype=content_type, filterby=filterby.lower())
+    object_dict = get_popular(ctype=content_type, filterby=filterby.lower())
 
-    return HttpResponse(
-        json, 
-        content_type = 'application/javascript; charset=utf8'
-    )
+    return render_to_response(template, {
+        'ctype': content_type,
+        'filterby': filterby,
+        'objects': object_dict.items(),
+    })
