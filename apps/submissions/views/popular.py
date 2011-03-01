47eea3fd74ba20a14742bf31a0527f0c9ccf44b8
Titus Soporan <tsopor@gmail.com>
2011-02-28 21:23:31 -0500

    Updated popular view and templates to use new code.

--- apps/submissions/views/popular.py
+++ apps/submissions/views/popular.py
@@ -7,11 +7,10 @@ import os
 from django.conf import settings
 
 def popular(request, ctype, filterby, template="submissions/popular/popular_load.html"):
-    content_type = get_object_or_404(ContentType, name__iexact=ctype)
-    object_dict = get_popular(ctype=content_type, filterby=filterby.lower())
+    popular_list = get_popular(ctype=ctype, filterby=filterby.lower())
 
     return render_to_response(template, {
-        'ctype': content_type,
+        'ctype': ctype,
         'filterby': filterby,
-        'objects': object_dict.items(),
+        'objects': popular_list,
     })
